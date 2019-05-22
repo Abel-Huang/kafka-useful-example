8fc3c6b8e229ffb4c51752aa241f3f925e05eb46
Abel-Huang <497720975@qq.com>
2019-05-23 00:34:54 +0800

    add project framework

---
+++ src/main/java/cn/abelib/mercury/Main.java
@@ -0,0 +1,11 @@
+package cn.abelib.mercury;
+
+/**
+ * @Author: abel.huang
+ * @Date: 2019-05-23 00:26
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+}
